2ecb78e69e086576f1f000a89b7c3d95058f307b
Christos <xrhstosdion@hotmail.com>
2014-12-11 19:00:43 +0100

    refactor
    refactor

--- src/refeus_executable.cpp
+++ src/refeus_executable.cpp
@@ -1,38 +1,19 @@
-<<<<<<< HEAD
 #ifdef _WIN32
-=======
-#ifdef win32
->>>>>>> origin/master
 #include <windows.h>
 #endif
 #ifdef unix
 #include <iostream>
 #endif
-<<<<<<< HEAD
 #include <c:\build\refeus_executable_updated\include\RefeusFunctionsDefinitions.hpp>
 
 #ifdef _WIN32
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow ) {
 
   RefeusProcess r;
-=======
-
-#include <RefeusProcess.hpp>
- 
-#ifdef win32
-int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow ) {
-#endif
-#ifdef unix
-int main(int argc, char** argv) {
-#endif
-  RefeusProcess r;
-  #ifdef win32
->>>>>>> origin/master
   char *command_line_cstr = GetCommandLine();
   std::string command_line = command_line_cstr;
   r.argParser(command_line);
   r.langCheck(GetUserDefaultLCID());
-<<<<<<< HEAD
   //r.start();
   //return r.start();
   //system("pause");
@@ -41,10 +22,6 @@ int main(int argc, char** argv) {
 #ifdef unix
 int main(int argc, char** argv) {
   RefeusProcess r;
-=======
-  #endif
-  #ifdef unix
->>>>>>> origin/master
   std::string command_line= "";
   for ( int i = 1
       ; i < argc
@@ -54,21 +31,12 @@ int main(int argc, char** argv) {
   }
   r.argParser(command_line);
   r.langCheck(1031);
-<<<<<<< HEAD
   
   //rp.setEnvironment("window_size", "400x800");
   //rip.setExecutable("node.exe");
   //rip.setExecutableParameter("app.js");
   r.start();
   //return r.start();
-=======
-  #endif
-  //rp.setEnvironment("window_size", "400x800");
-  //rip.setExecutable("node.exe");
-  //rip.setExecutableParameter("app.js");
-  //r.start();
-  return r.start();
->>>>>>> origin/master
   //rip.start();
   //system("pause");
  
