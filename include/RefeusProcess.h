534467d971c0cf0a8524c025a5d27c5deed39100
Christos <xrhstosdion@hotmail.com>
2015-02-03 11:43:38 +0100

    added function definitions

--- include/RefeusProcess.h
+++ include/RefeusProcess.h
@@ -6,7 +6,10 @@
 class RefeusProcess {
     private: std::map<std::string,std::string> environmentmap;
     private: std::string executable;
+    private: std::string selected_iso_language;
     private: std::vector<std::string> parametersvector;
+    private: bool portable;
+    
     private: bool debug;
     private: void _debug(std::string message,std::string title = "");
 
@@ -20,15 +23,20 @@ class RefeusProcess {
     private: void configureOpenRefeusDocument(std::string pathname);
     private: void configureSkipMaintenance(bool enabled);
     private: void configureStartupActivity(std::string activity_name);
+    private: void configureLanguageFromAPICode(int check);
     private: void configurePortable();
-    private: void usage();
+    private: void configureLanguageFromIsoString(std::string iso_language);
     private: void configureRefeusSettingsLocation(std::string refeus_set_location);
+    private: void usage();
+    
     
     public: RefeusProcess();
     public: bool argParser(std::string command_line);
-    public: void configureLanguageFromAPICode(int check);
-    public: void configureLanguageFromIsoString(std::string iso_language);
+    public: std::string configureBinPath();
+    public: void configureDriveType();
+    public: void configureLanguage();
     public: void setEnvironment(std::string env_name, std::string env_value);
     public: std::vector<std::string> &split(const std::string &string_to_split, const char delimiter_character, std::vector<std::string> &element_vector);
     public: int start();
+
 };
